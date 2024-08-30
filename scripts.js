c5b4c8e05c5b1449534a0a42360b399b19594754
sierrasotlar <sierrasotlarmuswagon@gmail.com>
2024-08-30 04:07:30 -0500

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function playMusic() {
+    alert("Playing Sierra's favorite song: Yung Lean - Kyoto!");
+    // You can replace this alert with actual music playback code.
+}
+
+document.getElementById('boyfriend-application').onsubmit = function(event) {
+    event.preventDefault();
+    alert("Thanks for applying! Sierra will review your application.");
+}
